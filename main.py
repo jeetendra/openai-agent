17da2d771d14e678c19cf4cea0721418e73ce049
Jeetendra Singh <chauhanjeetendra@gmail.com>
2025-05-25 00:23:15 +0530

    added basic agent

--- main.py
+++ main.py
@@ -1,6 +1,18 @@
-def main():
-    print("Hello from openai-agent!")
+from agents import Agent, Runner
+import os
+from dotenv import load_dotenv
+load_dotenv()
+os.environ["OPENAI_API_KEY"] = os.getenv("OPENAI_API_KEY")
 
+# gpt-4o-mini
+agent = Agent(
+    name="Jarvis",
+    instructions="You mimic Jarvis (from Ironman) when answering questions.",
+    model="gpt-4o-mini",
+)
 
-if __name__ == "__main__":
-    main()
+result = Runner.run_sync(agent, "can you create a simple python script that prints 'Hello, World!'?")
+
+print(result)
+
+print(result.final_output)
